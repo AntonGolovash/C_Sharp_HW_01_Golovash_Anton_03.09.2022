354fb8cfe4649a36d251c7b76adde0ddf660679d
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-09-04 16:22:01 +0300

    0

--- C_Sharp_HW_02_Golovash_Anton_03.09.2022/Program.cs
+++ C_Sharp_HW_02_Golovash_Anton_03.09.2022/Program.cs
@@ -10,11 +10,24 @@ namespace C_Sharp_HW_02_Golovash_Anton_03._09._2022
     {
         static void Main(string[] args)
         {
-            
+            /*
+            * 1. Даны целые положительные числа А, B, C.
+            * Значение этих чисел программа должна запрашивать у пользователя.
+            * На прямоугольнике размера А*В размещено максимально возможное
+            * количество квадратов со стороной С.
+            * Квадраты не накладываются друг на друга.
+            * Найти количество квадратов, размещенных на прямоугольнике,
+            * а также площадь незанятой части прямоугольника.
+            * Необходимо предусмотреть служебные сообщения в случае,
+            * если в прямоугольнике нельзя разместить ни одного квадрата
+            * со стороной С (например, если зна чение С превышает размер сторон прямоугольника).
+            */
             int a = 0;
             int b = 0;
             int c = 0;
+
         Start_of_the_program:
+
             Console.WriteLine("Enter the short side of the rectangle");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the long side of the rectangle");
@@ -29,7 +42,9 @@ namespace C_Sharp_HW_02_Golovash_Anton_03._09._2022
                 Console.WriteLine("Please, enter correct data");
                 goto Start_of_the_program;
             }
+
         Continuation:
+
             Console.WriteLine("Enter the side of a square inscribed in a rectangle");
             Console.WriteLine("The value must be less than the long side of the first rectangle");
             c = Convert.ToInt32(Console.ReadLine());
@@ -42,7 +57,9 @@ namespace C_Sharp_HW_02_Golovash_Anton_03._09._2022
                 Console.WriteLine("Please, enter correct data");
                 goto Continuation;
             }
+
         Continuation1:
+
             double vertical_division_result = (double)(a / c);
             double horizontal_division_result = (double)(b / c);
             double vertical_remainder_of_division = (double)(a % c);
@@ -52,7 +69,36 @@ namespace C_Sharp_HW_02_Golovash_Anton_03._09._2022
             Console.WriteLine($"On a rectangle, you can place {number_of_inscribed_squares} squares");
             Console.WriteLine($"Area of unoccupied part of the rectangle {area_of_unoccupied_part_of_rectangle} ");
 
+            /*
+            * 2. Начальный вклад в банке равен 10000 грн.
+            * Через каждый месяц размер вклада увеличивается на р процентов
+            * от имеющейся суммы (Р вещественное число, 0 <P < 25).
+            * Значение Р программа должна полу чать у пользователя.
+            * По данному Р определить через сколько месяцев размер вклада
+            * превысит 11000 грн., и вывести найденное количество месяцев К
+            * (целое число) и итоговый размер вклада S (вещественное число).
+            */
+
+            double deposit_amount = 10000;
+
+
+            /*
+            * 3. Даны целые положительные числа А и В (А < В).
+            * Вы вести все целые числа от А до В включительно;
+            * каждое число должно выводиться на новой строке;
+            * при этом каждое число должно выводиться количество раз,
+            * равное его значению (например, число 3 выводится 3 раза).
+            */
+
+            /*
+             * 4. Дано целое число N большее 0, найти число,
+             * полученное при прочтении числа N справа налево.
+             * Например, если было введено число 345,
+             * то программа должна вывести число 543.
+            */
+
             Console.ReadLine();
+
         }
     }
 }
